21e80d98ce4972e03f5443509ea9455eab34e82e
Chirag Kapoor <kapoorchirag690@gmail.com>
2026-03-30 13:38:59 +0530

    Adding code for multiple and named pointcuts

---
+++ src/main/java/com/springboot/learning/AOP/Service/Servicecode.java
@@ -0,0 +1,10 @@
+package com.springboot.learning.AOP.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Servicecode {
+    public void service(){
+        System.out.println("Service call");
+    }
+}
