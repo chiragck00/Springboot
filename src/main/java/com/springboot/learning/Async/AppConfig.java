906b220fce8117ed7c8001a7c99cc76cf784fc61
Chirag Kapoor <kapoorchirag690@gmail.com>
2026-04-05 15:46:17 +0530

    Adding custom threadpoolTaskexecutor

--- src/main/java/com/springboot/learning/Async/AppConfig.java
+++ src/main/java/com/springboot/learning/Async/AppConfig.java
@@ -2,19 +2,32 @@ package com.springboot.learning.Async;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AppConfig {
-    @Bean
+
     public ThreadPoolExecutor manualThreadPool(){
         int minPoolSize = 2;
         int maxPoolSize = 4;
         int queueSize = 5;
 
         return new ThreadPoolExecutor(minPoolSize, maxPoolSize, 1, TimeUnit.HOURS, new ArrayBlockingQueue<>(queueSize));
+    }
+
+    @Bean("customExecutor")
+    public Executor taskExecutor(){
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(3);
+        threadPoolTaskExecutor.setMaxPoolSize(5);
+        threadPoolTaskExecutor.setQueueCapacity(2);
+        threadPoolTaskExecutor.setThreadNamePrefix("My-Thread-Custom-");
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
     }
 }
