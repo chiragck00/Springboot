4dcf4e587e42ebd83e4e070c3cfe5b5495e80346
Chirag Kapoor <kapoorchirag690@gmail.com>
2026-04-05 15:56:53 +0530

    Adding out ThreadPoolExecutor

--- src/main/java/com/springboot/learning/Async/AppConfig.java
+++ src/main/java/com/springboot/learning/Async/AppConfig.java
@@ -4,20 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 public class AppConfig {
 
-    public ThreadPoolExecutor manualThreadPool(){
+    AtomicInteger counter = new AtomicInteger(1);
+
+    ThreadFactory customFactory = r -> {
+        Thread t = new Thread(r);
+        t.setName("My-thread-" + counter.getAndIncrement());
+        return t;
+    };
+
+    @Bean("customThreadPoolExecutor")
+    public Executor manualThreadPool(){
         int minPoolSize = 2;
         int maxPoolSize = 4;
         int queueSize = 5;
 
-        return new ThreadPoolExecutor(minPoolSize, maxPoolSize, 1, TimeUnit.HOURS, new ArrayBlockingQueue<>(queueSize));
+        return new ThreadPoolExecutor(minPoolSize, maxPoolSize, 1, TimeUnit.HOURS, new ArrayBlockingQueue<>(queueSize), customFactory);
     }
 
     @Bean("customExecutor")
---
+++ src/main/java/com/springboot/learning/Async/threadPoolExecutorCustom/AsyncControllerCustom.java
@@ -0,0 +1,17 @@
+package com.springboot.learning.Async.threadPoolExecutorCustom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AsyncControllerCustom {
+    @Autowired
+    AsyncServiceCustom asyncService;
+
+    @GetMapping("/getUser")
+    public void getUser(){
+        System.out.println("Inside controller" + Thread.currentThread().getName());
+        asyncService.updateUser();
+    }
+}
