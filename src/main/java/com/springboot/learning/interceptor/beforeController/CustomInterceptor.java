d442cc9192592d67af23dffa691dba562bcf4042
Chirag Kapoor <kapoorchirag690@gmail.com>
2026-04-07 13:27:02 +0530

    Adding interceptor before reaching controller

---
+++ src/main/java/com/springboot/learning/interceptor/beforeController/CustomInterceptor.java
@@ -0,0 +1,28 @@
+package com.springboot.learning.interceptor.beforeController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class CustomInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Pre handle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("Post handle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("After completion");
+    }
+}
