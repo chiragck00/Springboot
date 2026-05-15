bb0bb989331be21bf0beac5ca598c056e4768181
Chirag Kapoor <kapoorchirag690@gmail.com>
2026-05-15 17:05:55 +0530

    Adding many to many with Jointable, joinColumns, inverseJoinColumns

---
+++ src/main/java/com/springboot/learning/associations/ManyToMany/Uni/User.java
@@ -0,0 +1,56 @@
+package com.springboot.learning.associations.ManyToMany.Uni;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "USER_DETAILS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name, email;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+            @JoinTable(
+                    name = "USER_ORDER",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "order_id")
+            )
+    List<Order> orders = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
