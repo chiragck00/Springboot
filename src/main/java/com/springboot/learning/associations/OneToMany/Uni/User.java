8d9474e3faa3089cb5740d4eabe0d10d2cbac28a
Chirag Kapoor <kapoorchirag690@gmail.com>
2026-05-15 16:43:32 +0530

    Adding code for one to many unidirectional and testing orphan removal, fetch type eager

---
+++ src/main/java/com/springboot/learning/associations/OneToMany/Uni/User.java
@@ -0,0 +1,52 @@
+package com.springboot.learning.associations.OneToMany.Uni;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "USER_DETAILS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name, email;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    List<Order> orders = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
